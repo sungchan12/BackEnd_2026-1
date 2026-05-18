75c9d2a0dc780523c79f54fdde09a092e2a53858
sungchan12 <chanyouunng1@gmail.com>
2026-05-18 16:20:40 +0900

    feat: /introduce GET API name 파라미터 추가

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,9 +19,9 @@ public class HelloController {
     public String hello2() {
         return "hello";
     }
-
     @GetMapping("/introduce")
-    public String introduce() {
+    public String introduce(@RequestParam(required = false) String name, Model model) {
+        model.addAttribute("name", name);
         return "introduce";
     }
 }
